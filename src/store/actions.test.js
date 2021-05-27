91b2de16de75eb8450b57acd40e00b31c80515c0
Eva Garcia <emgg2222@gmail.com>
2021-05-27 19:48:37 +0200

    loginAction test

--- src/store/actions.test.js
+++ src/store/actions.test.js
@@ -2,13 +2,16 @@ import {
     authLoginFailure, 
     authLoginRequest, 
     authLoginSuccess,
-    advertsLoadedSuccess
+    advertsLoadedSuccess,
+    tagsLoadedSuccess, 
+    loginAction
  } from './actions';
 
 import { AUTH_LOGIN_REQUEST,
      AUTH_LOGIN_FAILURE,
      AUTH_LOGIN_SUCCESS,
-     ADVERTS_LOADED_SUCCESS
+     ADVERTS_LOADED_SUCCESS,
+     TAGS_LOADED_SUCCESS
      } from './types';
 
 describe('authLoginRequest', () =>{
@@ -47,7 +50,73 @@ describe('AdvertLoadedSuccess', () =>{
             payload: 'adverts'
         };
         const result = advertsLoadedSuccess(adverts); 
-        expect(result).toEqual(expectedAction)
+        expect(result).toEqual(expectedAction);
     })
 });
 
+describe('TagsLoadedSuccess', () =>{
+    test ('should return an TAGS_LOADED_SUCCESS action', ()=>{
+        const adverts = 'tags';
+        const expectedAction = {
+            type:TAGS_LOADED_SUCCESS,
+            payload: 'tags'
+        };
+        const result = tagsLoadedSuccess(adverts); 
+        expect(result).toEqual(expectedAction);
+    })
+});
+
+
+describe('LoginAction', () => {
+    describe('when login api resolves', () => {
+        const credentials = 'credentials';
+        const action = loginAction(credentials);
+        const dispatch = jest.fn();
+        const getState = () =>{};
+        const api = {
+            auth: { login: jest.fn().mockResolvedValue()}
+        };
+        const history = {
+                location: { },
+                replace: jest.fn()           
+        };
+        test('should dispatch and AUTH_LOGIN_REQUEST action', () => {
+            action(dispatch, getState, { api, history });
+            expect(dispatch).toHaveBeenCalledWith({type: AUTH_LOGIN_REQUEST});
+            //expect(dispatch).toHaveBeenCalled();
+        });
+
+        test('should call api.auth.login', () => {
+            action(dispatch, getState, { api, history });
+            expect(api.auth.login).toHaveBeenCalledWith(credentials);
+        });
+
+        test('should dispatch and AUTH_LOGIN_SUCCESS action', async () => {            
+            await action(dispatch, getState, { api, history });
+            expect(dispatch).toHaveBeenNthCalledWith(2, {type: AUTH_LOGIN_SUCCESS});
+        });
+
+        test('should redirect to /', async () => {        
+            await action(dispatch, getState, { api, history });
+            expect(history.replace).toHaveBeenCalledWith({ pathname: '/'});
+        });
+    })
+
+    describe('when login api throws', () => {
+        const credentials = 'credentials';
+        const action = loginAction(credentials);
+        const dispatch = jest.fn();
+        const error = 'Unauthorized';
+        const getState = () =>{};
+        
+        test('should dispatch and AUTH_LOGIN_FAILURE action', async () => {            
+            const api = {
+                auth: { login: jest.fn().mockRejectedValue(error)}
+            };
+            await action(dispatch, getState, { api });
+            expect(dispatch).toHaveBeenCalledWith({type: AUTH_LOGIN_FAILURE, payload: error, error:true});
+        });     
+
+    })
+   
+});
