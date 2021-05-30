b80d3ac1ecca3ceb9d72f072a5ebc76900c741ff
Eva Garcia <emgg2222@gmail.com>
2021-05-30 13:14:55 +0200

    add components test

---
+++ src/components/adverts/NewAdvertPage/NewAdvertForm.test.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import NewAdvertForm from './NewAdvertForm';
+
+describe('NEW ADVERT FORM', () => {
+    const props = {
+        onSubmit: jest.fn()
+    }
+
+    const render = () => shallow(<NewAdvertForm {...props} />);
+
+    test('should render', () => {
+        const wrapper = render ();
+        expect(wrapper.exists()).toBe(true);
+    })
+
+    test('should call submit', () => {
+        const wrapper = render();
+        const form = wrapper.find('form');
+        form.simulate('submit', {preventDefault: () => {}});
+        expect(props.onSubmit).toHaveBeenCalled();
+    })
+
+    test('should submit advert', () => {
+        const advert = {
+            name: 'Prod1',
+            sale: true,
+            price: 20, 
+            tags: ['tag1'], 
+            photo: null
+        }
+        const wrapper = render();
+        const nameField = wrapper.find('[name="name"]');
+        nameField
+            .props()
+            .onChange({target: {name: 'name', value: advert.name}});
+
+        const saleField = wrapper.find('[name="sale"]');
+        saleField
+            .props()
+            .onChange({target: {name: 'sale', value: advert.sale}});
+
+        const priceField = wrapper.find('[name="price"]');
+        priceField
+            .props()
+            .onChange({target: {name: 'price', value: advert.price}});
+        
+        const tagsField = wrapper.find('[name="tags"]');
+        tagsField
+            .props()
+            .onChange({target: {name: 'tags', value: advert.tags}});
+        
+        const photoField = wrapper.find('[name="photo"]');
+        photoField
+            .props()
+            .onChange({target: {name: 'photo', value: advert.photo}});
+    
+        const form = wrapper.find ('form');
+        form.simulate('submit', { preventDefault: () => {}});
+        expect(wrapper.find('button').props().disabled).toBe(false);
+        expect(props.onSubmit).toHaveBeenCalledWith(advert);       
+        
+    })
+
+});
---
+++ src/components/auth/LoginPage/LoginForm.test.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import LoginForm from './LoginForm';
+
+describe(' LOGIN FORM ', () => {
+    const props = {
+        onSubmit: jest.fn()
+    }
+
+    const render = () => shallow(<LoginForm {...props} />);
+
+    test('shoul render', ()=> {
+        const wrapper = render ();
+        expect(wrapper.exists()).toBe(true);
+    })
+
+    test('should call submit', () => {
+        const wrapper = render ();
+        const form = wrapper.find ('form');
+        form.simulate('submit', {preventDefault: () => {}});
+        expect(props.onSubmit).toHaveBeenCalled();
+    })
+
+    test('should submit credentials', () => {
+        const credentials = {
+            email: 'Eva', 
+            password: 'password',
+            remember: false
+        }
+        const wrapper = render ();
+        const emailField = wrapper.find('[name="email"]');
+        emailField
+            .props()
+            .onChange({target: {name :'email', value: credentials.email}})
+        const passwordField = wrapper.find('[name="password"]'); 
+        passwordField
+            .props()
+            .onChange({target: {name :'password', value: credentials.password}})
+        const rememberField = wrapper.find('[name="remember"]'); 
+        rememberField
+            .props()
+            .onChange({target: {name :'remember', value: credentials.remember}})
+        const form = wrapper.find ('form');
+        form.simulate('submit', {preventDefault: () => {}});
+        expect(wrapper.find('button').props().disabled).toBe(false);
+        expect(props.onSubmit).toHaveBeenCalledWith(credentials);
+    })
+
+})
