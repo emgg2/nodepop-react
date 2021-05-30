ce8c8585a5ec1e2b24493cbbb4c43ee2261f97e0
Eva Garcia <emgg2222@gmail.com>
2021-05-30 13:41:13 +0200

    add more snapshot testing

--- src/components/adverts/NewAdvertPage/NewAdvertForm.test.js
+++ src/components/adverts/NewAdvertPage/NewAdvertForm.test.js
@@ -62,9 +62,46 @@ describe('NEW ADVERT FORM', () => {
         expect(props.onSubmit).toHaveBeenCalledWith(advert);       
         
     }); 
-    
+
     test('snapshot testing', () => {
         const wrapper = render();
+        expect (wrapper).toMatchSnapshot();
+    }); 
+
+    test('snapshot testing with button disabled', () => {
+        const advert = {
+            name: 'Prod1',
+            sale: true,
+            price: 20, 
+            tags: ['tag1'], 
+            photo: null
+        }
+        const wrapper = render();
+        const nameField = wrapper.find('[name="name"]');
+        nameField
+            .props()
+            .onChange({target: {name: 'name', value: advert.name}});
+
+        const saleField = wrapper.find('[name="sale"]');
+        saleField
+            .props()
+            .onChange({target: {name: 'sale', value: advert.sale}});
+
+        const priceField = wrapper.find('[name="price"]');
+        priceField
+            .props()
+            .onChange({target: {name: 'price', value: advert.price}});
+        
+        const tagsField = wrapper.find('[name="tags"]');
+        tagsField
+            .props()
+            .onChange({target: {name: 'tags', value: advert.tags}});
+        
+        const photoField = wrapper.find('[name="photo"]');
+        photoField
+            .props()
+            .onChange({target: {name: 'photo', value: advert.photo}});
+    
         expect (wrapper).toMatchSnapshot();
     })
 
